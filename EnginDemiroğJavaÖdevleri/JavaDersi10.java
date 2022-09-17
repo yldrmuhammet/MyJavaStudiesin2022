33ae8c3d08ab2af7876636429345222b4a7b5feb
MUHAMMET YILDIRIM <muhammet.safir@gmail.com>
2022-09-17 20:37:00 +0300

    Create JavaDersi10.java

---
+++ EnginDemiroğJavaÖdevleri/JavaDersi10.java
@@ -0,0 +1,30 @@
+public class JavaDersi10 {
+    public static void main(String[] args) {
+
+        char grade = 'A';
+
+        switch (grade) {
+            case 'A':
+                System.out.println("Mükemmel    : Geçtiniz");
+                break;
+
+            case 'B':
+                System.out.println("Çok Güzel   : Geçtiniz");
+                break;
+
+            case 'C':
+                System.out.println("İyi         : Geçtiniz");
+                break;
+
+            case 'D':
+                System.out.println("Fena Değil  : Geçtiniz");
+                break;
+
+            case 'F':
+                System.out.println("Çok Güzel   : Geçtiniz");
+            default:
+                System.out.println("Geçersiz Not Girdiniz");
+        }
+
+    }
+}
