72d9f9c536e30f2bcb839f28540b2a0e92c6bfc9
MUHAMMET YILDIRIM <muhammet.safir@gmail.com>
2022-09-17 20:23:42 +0300

    Create JavaDersi8.java
    8

---
+++ EnginDemiroğJavaÖdevleri/JavaDersi8.java
@@ -0,0 +1,18 @@
+public class JavaDersi8 {
+    public static void main(String[] args) {
+
+        int sayi = 24;
+
+        if (sayi<20){
+            System.out.println("Sayı 20'den Küçüktür");
+        }
+
+        else if(sayi == 20){
+            System.out.println("Sayı 20'ye Eşittir");
+        }
+        else {
+            System.out.println("Sayı 20'den büyüktür.");
+        }
+        
+    }
+}
