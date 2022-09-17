1bb5ef3bb2fbe99078a436c3924e2985d1e6cc65
MUHAMMET YILDIRIM <muhammet.safir@gmail.com>
2022-09-17 20:48:32 +0300

    Create JavaDersi13.java

---
+++ EnginDemiroğJavaÖdevleri/JavaDersi13.java
@@ -0,0 +1,16 @@
+public class JavaDersi13 {
+    public static void main(String[] args) {
+
+        // Do-While
+        int i = 2;
+        do {
+            
+            System.out.println("Loglandı");
+            
+            System.out.println(i);
+            i += 2;
+
+        } while (i < 10);
+        System.out.println("Do-While Döngüsü Bitti");
+    }
+}
