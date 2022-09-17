ebd327a3802a8d1547cf61b239bf9a0e61a7385b
MUHAMMET YILDIRIM <muhammet.safir@gmail.com>
2022-09-17 20:39:53 +0300

    Create JavaDersi11.java

---
+++ EnginDemiroğJavaÖdevleri/JavaDersi11.java
@@ -0,0 +1,12 @@
+public class JavaDersi11 {
+    public static void main(String[] args) {
+        
+        // FOR
+
+        for(int i=2; i<10; i++){
+            System.out.println(i);
+        }
+        System.out.println("Döngü Bitti");
+
+    }
+}
