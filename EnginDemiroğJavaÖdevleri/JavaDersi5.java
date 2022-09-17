d8263d315f6c7611d0f956254dc30030e270c9a8
MUHAMMET YILDIRIM <muhammet.safir@gmail.com>
2022-09-17 20:06:10 +0300

    Create JavaDersi5.java
    asda

---
+++ EnginDemiroğJavaÖdevleri/JavaDersi5.java
@@ -0,0 +1,15 @@
+public class JavaDersi5 {
+    public static void Main(String[] args) {
+
+        int ogrenciSayisi = 12;
+        String mesaj = "Öğrenci Sayisi :";
+
+        System.out.println(mesaj + ogrenciSayisi);
+        System.out.println(mesaj + ogrenciSayisi);
+        System.out.println("Öğrenci Sayim" + ogrenciSayisi);
+        System.out.println("Öğrenci Sayim" + ogrenciSayisi);
+        System.out.println("Öğrenci Sayim "+ ogrenciSayisi);
+
+        
+    }
+}
