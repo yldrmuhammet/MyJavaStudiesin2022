f836dca6e3b8f0389d9467a0ac8b630f3342c3c1
MUHAMMET YILDIRIM <muhammet.safir@gmail.com>
2022-09-17 20:45:21 +0300

    Create JavaDersi12.java

---
+++ EnginDemiroğJavaÖdevleri/JavaDersi12.java
@@ -0,0 +1,20 @@
+public class JavaDersi12 {
+    public static void main(String[] args) {
+        
+        // For Döngüsü
+        for( int i = 2; i<10; i++){
+            System.out.println(i);
+        }
+        System.out.println("For Döngüsü Bitti");
+
+        int i=1;
+        //While Döngüsü
+        while(i<10){
+            System.out.println(i);
+            i++; 
+        }
+        
+        //İnfinity Loop
+        System.out.println("While Döngüsü Bitti");
+    }
+}
