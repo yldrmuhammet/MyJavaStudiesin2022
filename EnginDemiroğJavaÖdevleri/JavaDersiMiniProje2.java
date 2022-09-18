203d7366311f52032a93afd73459afbd3243cc7a
MUHAMMET YILDIRIM <muhammet.safir@gmail.com>
2022-09-18 11:47:41 +0300

    Create JavaDersiMiniProje2.java

---
+++ EnginDemiroğJavaÖdevleri/JavaDersiMiniProje2.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class JavaDersiMiniProje2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+         char harf = 'I';
+
+        
+        switch(harf){
+            case 'A':
+            case 'I':
+            case 'O':
+            case 'U':
+            System.out.println("Kalın Sesli Harf Girdiniz");
+            break;
+            case 'E':
+            case 'İ':
+            case 'Ö':
+            case 'Ü':
+            System.out.println("İnce Sesli Harf Girdiniz");
+            default:
+            System.out.println("Geçersiz bir değer girdiniz");
+        }
+
+        
+
+        input.close();
+    }
+}
